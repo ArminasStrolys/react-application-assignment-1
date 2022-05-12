b14810983b532273d3f256c1048b160e542b2130
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-12 22:44:48 +0300

    Nav bar layout
    Positioned the layout in navbar with colors and sizes

--- src/modules/app/App.js
+++ src/modules/app/App.js
@@ -4,11 +4,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>
-        <i style={{"color":'orange'}} className="fa-solid fa-person-digging"></i>
-        Under maintenance
-        <i style={{"color":'orange'}} className="fa-solid fa-person-digging"></i>
-      </h1>
       <Navi />
     </div>
   );
--- src/modules/navi/Navi.jsx
+++ src/modules/navi/Navi.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Navi = () => {
-    return (
-        <div>
-            TESTING
-        </div>
-    );
-}
+  return (
+    <div className="main-nav">
+      <form className="search-bar">
+        <input type="text" placeholder="Search..." />
+          <button className="button1">ONE</button>
+          <button className="button2">TWO</button>
+          <button className="button3">THREE</button>
+      </form>
+    </div>
+  );
+};
 
 export default Navi;
