1bf38f005b582d4dd132e640d9773d92b02c3dd6
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-12 15:09:44 +0300

    Created app
    Starting files set for further development.

---
+++ src/modules/app/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <i style={{"color":'orange'}} class="fa-solid fa-person-digging"></i>
+        Under maintenance
+        <i style={{"color":'orange'}} class="fa-solid fa-person-digging"></i>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
