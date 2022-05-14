676cd8cfdb5e082c6e27a7fc0a4b828fe65f27d8
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-14 23:43:50 +0300

    Getting ready to add functionality to buttons

--- src/modules/navi/Navi.jsx
+++ src/modules/navi/Navi.jsx
@@ -5,9 +5,9 @@ const Navi = () => {
     <div className="main-nav">
       <form className="search-bar">
         <input type="text" placeholder="Search..." />
-          <button className="button1">ONE</button>
-          <button className="button2">TWO</button>
-          <button className="button3">THREE</button>
+          <button className="button1">{`\u25B2`}ASCENDING</button>
+          <button className="button2">{`\u25BC`}DECENDING</button>
+          <button className="button3">By area/Oceania region</button>
       </form>
     </div>
   );
